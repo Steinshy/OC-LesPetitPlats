6d01814e611bfe60dd475c09c08bf87e2e464ed5
Steinshy <nekoheartdev@gmail.com>
2025-10-28 18:43:36 +0100

    Design: Medias for mobile / tablet / desktop. Responsive design. Add show more/less for filters. Placeholder for text/images. Cleanup code.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,3 @@
-/**
- * Main entry point
- * Imports global styles and initializes the application
- */
+/** Main entry point. Imports global styles and initializes the application. */
 import "../styles/global.css";
-
-// Your application logic will go here
-// The HTML structure is already in index.html
 console.info("Les Petits Plats application initialized");
